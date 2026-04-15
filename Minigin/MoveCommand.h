5dcbea8e29f38bcc35b1b00822b6217dc64b5ed5
Ana Carvalho <ana.paoliello.de.lucena.carvalho@student.howest.be>
2026-04-15 12:48:50 +0200

    Wall collision
    the player cant leave the designated area!

--- Minigin/MoveCommand.h
+++ Minigin/MoveCommand.h
@@ -36,7 +36,7 @@ namespace dae
                 float newY = pos.y + movement.y;
 
                 float spriteOffsetX = 4.0f;
-                float spriteOffsetY = 8.0f;
+                float spriteOffsetY = 10.0f;
 
 				// SNAP TO GRID
                 if (m_Direction.x != 0.0f)
@@ -50,6 +50,21 @@ namespace dae
                     newX = std::round((pos.x - spriteOffsetX) / m_GridSize) * m_GridSize + spriteOffsetX;
                 }
 
+				// WALL COLLISION
+                float minX = 0.0f;
+                float maxX = 1024.0f - m_GridSize;
+                float minY = m_OffsetY;
+                float maxY = 576.0f - m_GridSize;
+
+                // Clamp X
+                if (newX < minX) newX = minX;
+                if (newX > maxX) newX = maxX;
+
+                // Clamp Y
+                if (newY < minY) newY = minY;
+                if (newY > maxY) newY = maxY;
+
+				// Set the new position
                 m_pGameObject->SetLocalPosition(newX, newY);
 
                 // FLIPPING
